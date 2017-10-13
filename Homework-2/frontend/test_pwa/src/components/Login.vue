9c75ac844498f893b70e1a283872c9d1c9fa8dfd
renato <renato@cmpe272-lghr.com>
2017-10-13 01:42:11 -0700

    added Login component (route /Home); added dependecy (vue-local-storage)

---
+++ Homework-2/frontend/test_pwa/src/components/Login.vue
@@ -0,0 +1,47 @@
+<!-- Created by Yu Xu -->
+<template>
+  <div class="login">    
+    <!-- add here the SSO login button -->
+  <button v-on:click="loginTwitter()">Login with Twitter</button>
+  </div>
+</template>
+
+
+
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      loggedUser:{
+        name: 'John Doe',
+        friends_count: '240', // following
+        followers_count: '10', // followers
+        id_str: ''
+      },      
+    }
+  },  
+  methods: {
+    loginTwitter: function(msg) {
+        // add here code that call the Twitter API to login
+    },    
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+
+
+
+
+a {
+  color: #35495E;
+}
+</style>
--- Homework-2/frontend/test_pwa/src/router/index.js
+++ Homework-2/frontend/test_pwa/src/router/index.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Profile from '@/components/Profile'
+import Login from '@/components/Login'
 import Logout from '@/components/Logout'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -21,7 +22,7 @@ export default new Router({
     {
       path: '/Home',
       name: 'Home',
-      component: Profile
+      component: Login
     },
     {
       path: '/Tweet',
@@ -40,3 +41,39 @@ export default new Router({
     },
   ]
 })
+
+
+router.beforeEach((to, from, next) => {
+    console.log("to.path=" + to.path)
+    if(to.path != '/' ){
+      console.log("entrou - ")
+      
+     
+      if(localStorage.getItem('user')==undefined){
+        console.log("don't exist")
+
+      }
+            //prompt for username
+        //     var user = prompt('please enter your username');
+        //     //prompt for password
+        //     var pass = prompt('please enter your password');
+        //     //check if th username and password given equals our preset details
+        //     if (user == 'username' && pass == 'password'){
+        //     //set the user item
+        //     localStorage.setItem('user', user);
+        //     //move to the route
+        //     next();
+        //     }else{
+        //     //alert the username and pass is wrong
+        //     alert('Wrong username and password, you do not have permission to access that route');
+        //     //return, do not move to the route
+        //     return;
+        //     }
+
+        
+    }
+
+    next()
+})
+
+export default router
